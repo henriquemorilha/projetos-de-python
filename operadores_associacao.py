c5e5da216fd86e9d5ddd5bfbd69eb820deac80d2
Henrique Morilha <henriquemorilha97@gmail.com>
2025-11-22 03:28:53 -0300

    operadores-associacao

---
+++ operadores_associacao.py
@@ -0,0 +1,10 @@
+frutas = ["limao", "uva"]
+curso = "Curso de Python"
+
+print("laranja" in frutas)
+print("Python" in curso)
+
+frutas = ["limao", "uva", "laranja"]
+
+print("laranja" in frutas)
+print("limao" not in frutas)
