74d902ca2f739c768aae4a0a1c9b700563ede6dc
Henrique Morilha <henriquemorilha97@gmail.com>
2025-11-23 01:31:03 -0300

    string-multiplas-linhas

---
+++ string_multiplas_linhas.py
@@ -0,0 +1,23 @@
+descricao = """ 
+Este é um programa em Python
+que demonstra o uso de strings
+em múltiplas linhas.
+"""
+
+print(descricao)
+
+
+menu = """
+========== MENU ==========
+1 - Sacar
+2 - Depositar
+3 - Ver saldo
+4 - Sair
+==========================
+
+Escolha uma opção: 
+"""
+
+opcao = input(menu)
+
+print(f"Você escolheu a opção {opcao}.")
