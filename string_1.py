ecfc30e2d3fa83e73c0f2fe6242e0ea7265e62c4
Henrique Morilha <henriquemorilha97@gmail.com>
2025-11-22 23:51:59 -0300

    string-2

--- string_1.py
+++ string_1.py
@@ -1,20 +1,7 @@
 nome = "hEnrIqUe"
 
-print(nome.upper())
-print(nome.lower())
-print(nome.title())
-
-texto = "  Olá mundo!  "
-
-print(texto + ".")
-print(texto.strip() + ".")
-print(texto.rstrip() + ".")
-print(texto.lstrip() + ".")
-
-menu = "Python"
-
-print("####" + menu + "####")
-print(menu.center(14))
-print(menu.center(14, "#"))
-print("P-y-t-h-o-n")
-print("-".join(menu))
+print(nome.upper())   # HENRIQUE
+print(nome.lower())   # henrique
+print(nome.title())   # Henrique
+print(nome.capitalize())  # Henrique (só a primeira letra)
+print(nome.swapcase())    # HeNrIqUe (inverte maiúsc/minúsc)
