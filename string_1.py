ee70afa27327c188f356794f8029cb0f2cf2d998
Henrique Morilha <henriquemorilha97@gmail.com>
2025-11-22 23:44:42 -0300

    string-1

---
+++ string_1.py
@@ -0,0 +1,12 @@
+nome = "hEnrIqUe"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto = "  Olá mundo!  "
+
+print(texto + ".")
+print(texto.strip() + ".")
+print(texto.rstrip() + ".")
+print(texto.lstrip() + ".")
